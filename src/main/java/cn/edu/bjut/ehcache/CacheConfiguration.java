92f4cb4800bb0368258fb44e791a9b29d7aa5d7c
hanxiao11 <hanxiao11@jd.com>
2017-07-03 18:39:05 +0800

    ehcache

---
+++ src/main/java/cn/edu/bjut/ehcache/CacheConfiguration.java
@@ -0,0 +1,29 @@
+package cn.edu.bjut.ehcache;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+/**
+ * Created by hanxiao11 on 2017/7/3.
+ */
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+    @Bean
+    public EhCacheCacheManager ehCacheCacheManager(EhCacheManagerFactoryBean bean) {
+        return new EhCacheCacheManager(bean.getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheManagerFactoryBean() {
+        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
+        cacheManagerFactoryBean.setConfigLocation(new ClassPathResource("conf/ehcache.xml"));
+        cacheManagerFactoryBean.setShared(true);
+        return cacheManagerFactoryBean;
+    }
+}
